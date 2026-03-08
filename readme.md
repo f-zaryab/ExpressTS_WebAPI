a7543ea3e20b8006be4dae8b2f4375e277822857
f-zaryab <zaryabfarooqui@gmail.com>
2026-03-08 01:50:09 +0000

    added cors and notfound handling

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Express-TS Repo
+
+This is a template repo for setting up Express.js in typescript. This repo mimics an average express repo with middlewares, Routes, and Controllers to form a fully functional web api.
+
+Additionally fully-typed CORS handling is also incorporated.
+
+## Installing Express(TS)
+
+- Initiating Package.json: npm init -y
+- Installing Express.js: npm i express
+- Installing typescript: npm i -D typescript
+- Installing typescript's types: npm i -D @types/express
+- Installing a default tsconfig package: npm i -D @tsconfig/node22
+
+**Note**: Use node version 22.18 or greater for this setup to work. Otherwise needs to compile ts to js and start server from dist folder.
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,29 @@
 import express from "express";
-import type { Express } from "express";
+import cors from "cors";
+import type { Express, Request, Response } from "express";
+import { pets } from "./data/pets.ts";
 
 const app: Express = express();
-
 const PORT = process.env.PORT || 5001;
 
-type Pet = {
-  name: string;
-  species: string;
-  adopted: boolean;
-  age: number;
-};
+// Handling CORS
+app.use(cors());
 
-const pet: Pet = {
-  name: "Rubik",
-  species: "Cat",
-  adopted: true,
-  age: 3,
-};
-
+// Routes
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to PET-Api" });
 });
 
-app.get("/pet", (req, res) => {
-  res.json({ data: pet });
+app.get("/pet", (req: Request, res: Response): void => {
+  res.json({ data: pets });
 });
 
+// Not-Found Route
+app.use((req: Request, res: Response): void => {
+  res.status(404).json({ message: "No route found" });
+});
+
+// Server
 app.listen(PORT, (): void => {
   console.log(`App is listening on PORT: ${PORT}`);
 });
